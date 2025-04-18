117da93f1a6a1cd650c1cab7d2ef96aaa47f1908
JoanneOs <jowan.awsman@yahoo.com>
2025-04-17 22:08:21 -0500

    ObjectId needs to be instantiated

--- db/conn.mjs
+++ db/conn.mjs
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const client = new MongoClient(process.env.ATLAS_URI);
-
 let conn;
 try {
   conn = await client.connect();
@@ -11,6 +10,6 @@ try {
   console.error(e);
 }
 
+// Connect to the "sample_training" database
 let db = conn.db("sample_training");
-
 export default db;
--- index.mjs
+++ index.mjs
@@ -1,25 +1,34 @@
 // creating new project:
 //copied same steps from CRUD yesterday example
 
+//copied las
+
+// creating new project:
+//copied same steps from CRUD yesterday example
+
 //copied last codes form slack
 
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import grades from "./routes/grades.mjs"
+import grades from "./routes/grades.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(express.json());
 
+// Root route
 app.get("/", (req, res) => {
   res.send("Welcome to the API.");
 });
+
+// API routes
 app.use("/grades", grades);
 
-// Global error handling
+// Global error handler
 app.use((err, _req, res, next) => {
+  console.error(err.stack); // Log the error for debugging
   res.status(500).send("Seems like we messed up somewhere...");
 });
 
