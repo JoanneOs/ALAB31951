473a43fc2f89d0ca245334297d445b7be29b5632
JoanneOs <jowan.awsman@yahoo.com>
2025-04-19 16:58:08 -0500

    Grade.aggregate() instead of collection.aggregate()

--- routes/grades_agg.mjs
+++ routes/grades_agg.mjs
@@ -1,6 +1,7 @@
 import express from "express";
-import db from "../db/conn.mjs";
-import { ObjectId } from "mongodb";
+//import db from "../db/conn.mjs";
+//import { ObjectId } from "mongodb";
+import Grade from "../models/Grade.mjs"
 
 const router = express.Router();
 
@@ -20,66 +21,99 @@ const router = express.Router();
 
 // Get the weighted average of a specified learner's grades, per class
 router.get("/learner/:id/avg-class", async (req, res) => {
-  let collection = await db.collection("grades");
+  // let collection = await db.collection("grades");
 
-  let result = await collection
-    .aggregate([
-      {
-        $match: { learner_id: Number(req.params.id) },
-      },
-      {
-        $unwind: { path: "$scores" },
-      },
+  // let result = await collection
+  //   .aggregate([
+  //     {
+  //       $match: { learner_id: Number(req.params.id) },
+  //     },
+  //     {
+  //       $unwind: { path: "$scores" },
+  //     },
+  //     {
+  //       $group: {
+  //         _id: "$class_id",
+  //         quiz: {
+  //           $push: {
+  //             $cond: {
+  //               if: { $eq: ["$scores.type", "quiz"] },
+  //               then: "$scores.score",
+  //               else: "$$REMOVE",
+  //             },
+  //           },
+  //         },
+  //         exam: {
+  //           $push: {
+  //             $cond: {
+  //               if: { $eq: ["$scores.type", "exam"] },
+  //               then: "$scores.score",
+  //               else: "$$REMOVE",
+  //             },
+  //           },
+  //         },
+  //         homework: {
+  //           $push: {
+  //             $cond: {
+  //               if: { $eq: ["$scores.type", "homework"] },
+  //               then: "$scores.score",
+  //               else: "$$REMOVE",
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //     {
+  //       $project: {
+  //         _id: 0,
+  //         class_id: "$_id",
+  //         avg: {
+  //           $sum: [
+  //             { $multiply: [{ $avg: "$exam" }, 0.5] },
+  //             { $multiply: [{ $avg: "$quiz" }, 0.3] },
+  //             { $multiply: [{ $avg: "$homework" }, 0.2] },
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   ])
+  //   .toArray();
+
+  // if (!result) res.send("Not found").status(404);
+  // else res.send(result).status(200);
+
+  try {
+    const result = await Grade.aggregate([
+      { $match: { learner_id: Number(req.params.id) } },
+      { $unwind: "$scores" },
       {
         $group: {
           _id: "$class_id",
-          quiz: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "quiz"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
-              },
-            },
-          },
-          exam: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "exam"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
-              },
-            },
-          },
-          homework: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "homework"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
-              },
-            },
-          },
+          quiz: { $avg: { $cond: [{ $eq: ["$scores.type", "quiz"] }, "$scores.score", null] } },
+          exam: { $avg: { $cond: [{ $eq: ["$scores.type", "exam"] }, "$scores.score", null] } },
+          homework: { $avg: { $cond: [{ $eq: ["$scores.type", "homework"] }, "$scores.score", null] } },
         },
       },
       {
         $project: {
-          _id: 0,
           class_id: "$_id",
           avg: {
             $sum: [
-              { $multiply: [{ $avg: "$exam" }, 0.5] },
-              { $multiply: [{ $avg: "$quiz" }, 0.3] },
-              { $multiply: [{ $avg: "$homework" }, 0.2] },
+              { $multiply: ["$exam", 0.5] },
+              { $multiply: ["$quiz", 0.3] },
+              { $multiply: ["$homework", 0.2] },
             ],
           },
         },
       },
-    ])
-    .toArray();
+    ]);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result.length) res.status(404).send("Not found");
+    else res.status(200).send(result);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+
 });
 
 export default router;
