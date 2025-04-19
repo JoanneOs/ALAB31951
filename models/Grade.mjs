1a397927df34959566f07a630ac86a843d46c142
JoanneOs <jowan.awsman@yahoo.com>
2025-04-19 16:53:28 -0500

    mongoose.Schema for validation. and Exports a Grade model for database operations.

--- models/Grade.mjs
+++ models/Grade.mjs
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
 
+const scoreSchema = new mongoose.Schema({
+    type: { type: String, required: true }, // "exam", "quiz", "homework"
+    score: { type: Number, required: true },
+  });
+  
+  const gradeSchema = new mongoose.Schema({
+    learner_id: { type: Number, required: true },
+    class_id: { type: Number, required: true },
+    scores: [scoreSchema], // Array of scores
+  });
+  
+
+
 const Grade = mongoose.model("Grade", gradeSchema);
 export default Grade;
